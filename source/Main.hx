6d41ab3b8a913813ccbaf0e84a7e05428a03163e
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-12-09 22:00:31 +0200

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -1,15 +1,12 @@
 package;
 
-import flixel.system.scaleModes.RatioScaleMode;
 import flixel.FlxG;
 import flixel.FlxGame;
-import openfl.display.FPS;
 import openfl.display.Sprite;
 
 class Main extends Sprite
 {
-	public static var fpsDisplay:FPS;
-
+	public static var fpsDisplay:Overlay;
 	public static var novid:Bool = false;
 	public static var flippymode:Bool = false;
 
@@ -17,15 +14,19 @@ class Main extends Sprite
 	{
 		super();
 
+		SUtil.uncaughtErrorHandler();
+
 		#if sys
 		novid = Sys.args().contains("-novid");
 		flippymode = Sys.args().contains("-flippymode");
 		#end
 
+		SUtil.checkPermissions();
+
 		addChild(new FlxGame(1280, 720, Startup, 1, 144, 144, true));
 
 		#if !mobile
-		fpsDisplay = new FPS(10, 3, 0xFFFFFF);
+		fpsDisplay = new Overlay(10, 3);
 		fpsDisplay.visible = true;
 		addChild(fpsDisplay);
 		#end
